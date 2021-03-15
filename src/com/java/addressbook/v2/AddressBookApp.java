1c43566a73a1643a1a612d2a4c26fa7b1452fa5e
김상민 <wrt123123123@gmail.com>
2021-03-16 08:50:09 +0900

    AddressBook v2

---
+++ src/com/java/addressbook/v2/AddressBookApp.java
@@ -0,0 +1,8 @@
+package com.java.addressbook.v2;
+
+public class AddressBookApp {
+	public static void main(String[] args) {
+		AddressBookController adr = new AddressBookController();
+		adr.addressBook();
+	}
+}
--- src/com/java/addressbook/v2/Loop.java
+++
@@ -1,5 +0,0 @@
-package com.java.addressbook.v2;
-
-public class Loop {
-
-}
